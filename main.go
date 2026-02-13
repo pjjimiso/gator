5ba0ff653c0d132a25df606528240bbbe8241548
Patrick Jimison <patrick.j.jimison@intel.com>
2026-02-13 14:54:04 -0700

    Add follow and following handlers to registered commands

--- main.go
+++ main.go
@@ -46,6 +46,8 @@ func main() {
 	cmds.register("agg", handlerAgg)
 	cmds.register("addfeed", handlerAddFeed)
 	cmds.register("feeds", handlerFeeds)
+	cmds.register("follow", handlerFollow)
+	cmds.register("following", handlerFollowing)
 
 	cmdName := os.Args[1]
 	cmdArgs := os.Args[2:]
