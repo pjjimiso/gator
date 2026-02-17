7a9348a0cbd713e404a953186e9ffe10f260cad4
Patrick Jimison <patrick.j.jimison@intel.com>
2026-02-17 11:09:13 -0700

    Wrap handlers with middlewareLoggedIn function

--- main.go
+++ main.go
@@ -44,10 +44,10 @@ func main() {
 	cmds.register("reset", handlerReset)
 	cmds.register("users", handlerUsers)
 	cmds.register("agg", handlerAgg)
-	cmds.register("addfeed", handlerAddFeed)
+	cmds.register("addfeed", middlewareLoggedIn(handlerAddFeed))
 	cmds.register("feeds", handlerFeeds)
-	cmds.register("follow", handlerFollow)
-	cmds.register("following", handlerFollowing)
+	cmds.register("follow", middlewareLoggedIn(handlerFollow))
+	cmds.register("following", middlewareLoggedIn(handlerFollowing))
 
 	cmdName := os.Args[1]
 	cmdArgs := os.Args[2:]
@@ -60,3 +60,4 @@ func main() {
 		log.Fatal(err)
 	}
 }
+
