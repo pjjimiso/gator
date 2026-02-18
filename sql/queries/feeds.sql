01d7f42eae383c7abb53bf470b11fa95bd395277
Patrick Jimison <patrick.j.jimison@intel.com>
2026-02-18 09:01:37 -0700

    Add last_fetched_at column to feeds table, add now MarkFeedFetched query to update the fetch date of a given feed

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -1,18 +1,18 @@
 -- name: CreateFeed :one
 INSERT INTO feeds (
-	id,
-	created_at,
-	updated_at,
-	name,
-	url,
-	user_id
+  id,
+  created_at,
+  updated_at,
+  name,
+  url,
+  user_id
 ) VALUES (
-	$1,
-	$2,
-	$3,
-	$4,
-	$5,
-	$6
+  $1,
+  $2,
+  $3,
+  $4,
+  $5,
+  $6
 )
 RETURNING *;
 
@@ -24,3 +24,11 @@ INNER JOIN users ON feeds.user_id = users.id;
 -- name: GetFeed :one
 SELECT * FROM feeds
 WHERE url = $1;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET
+  last_fetched_at = NOW(),
+  updated_at      = NOW()
+WHERE id = $1;
+
